0b1f066bac85d14c3e42fb9d3ce1f32b2b92f4f7
divyalakshmikt <divyalakshmikt@gmail.com>
2024-09-03 15:39:12 +0530

    code test 2

--- src/controllers/whatsupController.js
+++ src/controllers/whatsupController.js
@@ -1,5 +1,17 @@
 const verifyToken =(req,res)=>{
-    res.send("Hello Verify token")
+    try {
+        let accessToken= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+        let token = req.query["hub.verify_token"];
+        let challenge = req.query["hub.challenge"]
+        if(challenge != null && token != null && token== accessToken){
+            res.send(challenge)
+        }else{
+            res.status(400).send()
+        }
+
+    } catch (error) {
+        res.status(400).send()
+    }
 }
 
 const RecivedMessage =(req,res)=>{
